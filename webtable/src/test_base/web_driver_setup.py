e31a77e6424c995205f11a1338c5183e722bd05d
Ernesto Albarez <ernesto.albarez@protonmail.com>
2023-02-12 22:38:34 -0300

    remove comments

--- webtable/src/test_base/web_driver_setup.py
+++ webtable/src/test_base/web_driver_setup.py
@@ -1,12 +1,9 @@
 import unittest
-
-import urllib3
 from selenium import webdriver
 
 
 class WebDriverSetup(unittest.TestCase):
     def setUp(self):
-        # urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(10)
         self.driver.maximize_window()
